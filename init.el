f447e07631ba055e2cb6e2beae795f6b8015ecaf
andre <ank2.qc@gmail.com>
2023-07-14 09:27:32 -0300

    list buffer

--- init.el
+++ init.el
@@ -401,6 +401,17 @@ A single-digit prefix argument gives the top window arg*10%."
   :bind (:map projectile-mode-map
 	      ("C-c p" . projectile-command-map)))
 
+(use-package web-mode
+  :ensure t)
+
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
 (use-package undo-tree
   :ensure t
   :config
